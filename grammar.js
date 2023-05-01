4718d564141028c18288af5c7efbe52a13899df2
mkimmj <103452017+mkimmj@users.noreply.github.com>
2023-04-30 19:30:35 -0700

    grammar print to console

--- grammar.js
+++ grammar.js
@@ -5,7 +5,7 @@ function capitalize (string){return  string.replace(/^\w/, c => c.toUpperCase())
 
 let grammars = {
     GenerationSimple : function (gram){
-        //console.log(gram)
+        console.log(gram)
         g = tracery.createGrammar(gram);
         return g.flatten("#origin#")
     }
